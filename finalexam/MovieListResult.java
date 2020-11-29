74bc1692edd80083b4844f70773eb742b3242cf9
sonnonet <korea.sonnonet@gmail.com>
2020-11-30 02:39:57 +0900

    Create MovieListResult.java

---
+++ finalexam/MovieListResult.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+
+}
