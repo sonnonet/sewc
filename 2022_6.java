27972a748652e529deb570565d1ddecac77d3d67
sonnonet <korea.sonnonet@gmail.com>
2022-10-04 15:38:37 +0900

    Update 2022_6.java

--- 2022_6.java
+++ 2022_6.java
@@ -1,50 +1,17 @@
-        myDBHelper = new myDBHelper(this);
-        btnInit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sqlDB = myDBHelper.getWritableDatabase();
-                myDBHelper.onUpgrade(sqlDB,1,2);
-                sqlDB.close();
-            }
-        });
-        btnInsert.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("WrongConstant")
-            @Override
-            public void onClick(View v) {
-                sqlDB = myDBHelper.getWritableDatabase();
-                sqlDB.execSQL("INSERT INTO user1 VALUES ( '" + edtName.getText().toString() + "' , '" + edtPhoneNumber.getText().toString() + "' ,  '" + edtEmail.getText().toString() + "', " + edtSecure.getText().toString() + " , '" + edtAddress.getText().toString() + "' )");
-                sqlDB.close();
-                Toast.makeText(getApplicationContext(),"입력됨",0).show();
-            }
-        });
+    public class myDBHelper extends SQLiteOpenHelper {
+        public myDBHelper(Context context) {
+            super(context, "groupDB", null, 1);
+        }
 
-        btnSelect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sqlDB = myDBHelper.getReadableDatabase();
-                Cursor cursor;
-                cursor = sqlDB.rawQuery("SELECT * FROM user1;",null);
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE user1 ( gName CHAR(20), gPhoneNumber CHAR(20), gEmailAddress CHAR(40) PRIMARY KEY,gSecure INTEGER, gAddress CHAR(100));");
+        }
 
-                String strNames = "이름"+"\r\n"+"\r\n";
-                String strPhoneNumbers = "핸드폰번호"+"\r\n"+"\r\n";
-                String strEmail = "이메일 주소"+"\r\n"+"\r\n";
-                String strSecure = "비밀번호"+"\r\n"+"\r\n";
-                String strAddress = "집 주소"+"\r\n"+"\r\n";
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS user1");
+            onCreate(db);
 
-                while (cursor.moveToNext()){
-                    strNames += cursor.getString(0) + "\r\n";
-                    strPhoneNumbers += cursor.getString(1) + "\r\n";
-                    strEmail += cursor.getString(2) + "\r\n";
-                    strSecure += cursor.getString(3) + "\r\n";
-                    strAddress += cursor.getString(4) + "\r\n";
-
-                }
-                edtNameResult.setText(strNames);
-                edtPhoneNumberResult.setText(strPhoneNumbers);
-                edtEmailResult.setText(strEmail);
-                edtSecureResult.setText(strSecure);
-                edtAddressResult.setText(strAddress);
-                cursor.close();
-                sqlDB.close();
-            }
-        });
+        }
+    }
